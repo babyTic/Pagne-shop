cbdef0eb4a537cb8aa0745d240ab5cbe4a5a2fc2
babyTic <armand2dakoure@gmail.com>
2026-05-14 08:33:53 +0000

    fix: update api url for production

--- pagne-frontend/src/services/api.js
+++ pagne-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "pagne-shop-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
     "Accept":       "application/json", // important pour Laravel
